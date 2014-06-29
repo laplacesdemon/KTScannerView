db7ed0993ea58520e1d82ab713adbeeedb9d2eed
Suleyman Melikoglu <suleymanmelikoglu@gmail.com>
2014-06-29 12:13:27 +0300

    trivial changes

--- KTScannerView/KTScannerView.h
+++ KTScannerView/KTScannerView.h
@@ -1,6 +1,5 @@
 //
 //  KTScannerView.h
-//  BPlus
 //
 //  Created by Suleyman Melikoglu on 28/06/14.
 //  Copyright (c) 2014 Katu. All rights reserved.
--- KTScannerView/KTScannerView.m
+++ KTScannerView/KTScannerView.m
@@ -1,6 +1,5 @@
 //
 //  KTScannerView.m
-//  BPlus
 //
 //  Created by Suleyman Melikoglu on 28/06/14.
 //  Copyright (c) 2014 Katu. All rights reserved.
